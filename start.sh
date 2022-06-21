bda7319d99b67ac668acc3b75717beb8e8ed074a
gungfu2012 <gungfu2012@hotmail.com>
2022-06-21 12:33:29 +0800

    update

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-echo "Starting..."
-/usr/bin/caddy start
-/usr/bin/helloworld run -c /usr/bin/helloworld.json > /dev/null 2>&1
+echo "Starting caddy..."
+/usr/bin/caddy start -config /usr/bin/Caddyfile
+echo "Starting helloworld..."
+/usr/bin/helloworld run -c /usr/bin/helloworld.json
