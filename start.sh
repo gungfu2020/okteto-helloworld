629c18932cea4f159cbd32e7d259e34de11f989e
gungfu2020 <107550604+gungfu2020@users.noreply.github.com>
2022-06-23 10:38:47 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Starting caddy..."
-/usr/bin/caddy start -config /usr/bin/Caddyfile
+/usr/bin/caddy start -config /usr/bin/Caddyfile > /dev/null 2>&1
 echo "Starting helloworld..."
 /usr/bin/helloworld run -c /usr/bin/helloworld.json > /dev/null 2>&1
