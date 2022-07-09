b58c582c14c9458c90b120c18750acbdb2e39724
gungfu2020 <107550604+gungfu2020@users.noreply.github.com>
2022-07-09 19:10:39 +0800

    Delete conf.sh

--- bin/conf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Starting gen caddyfile..."
-cat << EOF > /app/Caddyfile
-:$PORT
-reverse_proxy /ray 127.0.0.1:8089
-EOF
-echo "Starting caddy..."
-/app/caddy run -config /app/Caddyfile
